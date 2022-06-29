e8b7a3fbc5d56990dcae0cd1616c227991b59d3b
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-06-29 13:46:19 +0000

    Add promise checks for fetching repo data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -35,10 +35,14 @@ function fetchGitHubInformation(event) {
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then (
         function(response) {
-            var userData = response;
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
